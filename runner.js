ded4d0df95e3c28fbf1f93db8c2e6dec53906c29
peter36 <peter0306@gmail.com>
2016-07-21 20:02:43 -0700

    Update path

--- runner.js
+++ runner.js
@@ -2,10 +2,12 @@
 
 var adventure = require('adventure');
 var shop = adventure('javascript-adventure');
+var path = require('path');
 
 var problems = [ '01_Hello_World', '02_Functions' ];
 problems.forEach(function (prob) {
-    shop.add(prob.replace(/_/g, ' '), function () { return require('./problems/' + prob.toLowerCase())});
+    shop.add(prob.replace(/_/g, ' '), function () { 
+        return require(path.join(__dirname, '/problems/' + prob.toLowerCase()))});
 });
 
 shop.execute(process.argv.slice(2));
