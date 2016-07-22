b6e91433d52eeef6aeb3e52d7b1f52edd235520b
peter36 <peter0306@gmail.com>
2016-07-21 17:08:08 -0700

    Convert _ to space for the menu

--- runner.js
+++ runner.js
@@ -3,9 +3,9 @@
 var adventure = require('adventure');
 var shop = adventure('javascript-adventure');
 
-var problems = [ '01_hello_world', '02_functions' ];
+var problems = [ '01_Hello_World', '02_Functions' ];
 problems.forEach(function (prob) {
-    shop.add(prob, function () { return require('./problems/' + prob) });
+    shop.add(prob.replace(/_/g, ' '), function () { return require('./problems/' + prob.toLowerCase())});
 });
 
 shop.execute(process.argv.slice(2));
